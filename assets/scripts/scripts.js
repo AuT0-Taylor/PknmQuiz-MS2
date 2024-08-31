d139df032a3e8d3cc02caf6785eed6a732d4682c
harri taylor <harriworktaylor@gmail.com>
2024-08-31 06:16:47 +0000

    add function to quiz giving new question each time

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -8,8 +8,8 @@ let score = 0;
 let questionCounter = 0;
 let availableQuesions = [];
 
-const Correct_Point = 5;
-const Max_Question = 5;
+const Correct_Point = 10;
+const Max_Question = 3;
 
 /* questions */
 let questions = [
@@ -22,22 +22,60 @@ let questions = [
         answer: 1,
     },
     {
-        question: "?",
-        choice1: "",
-        choice2: "",
-        choice3: "",
-        choice4: "",
-        answer: 1,
+        question: "What type is effective against ground?",
+        choice1: "electric",
+        choice2: "steel",
+        choice3: "fairy",
+        choice4: "water",
+        answer: 4,
     },
     {
-        question: "?",
-        choice1: "",
-        choice2: "",
-        choice3: "",
-        choice4: "",
-        answer: 1,
+        question: "What type is super effective against dragon?",
+        choice1: "dragon",
+        choice2: "flying",
+        choice3: "fairy",
+        choice4: "poison",
+        answer: 3,
     },
 ];
 
 /*Functions*/
 
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuesions = [...questions];
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    /*finished quiz goes to end html*/
+    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        return window.location.assign('/end.html');
+    }
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach( (choice) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    } );
+
+    availableQuesions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+
+choices.forEach( (choice) => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+        getNewQuestion();
+    } );
+} );
+
+startGame();
