296d4c06ef7604753506a0705dc6cccdf6c416cc
harri taylor <harriworktaylor@gmail.com>
2024-08-31 05:51:19 +0000

    question array template

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,27 @@
+/* questions */
+let questions = [
+    {
+        question: "whats effective against water?",
+        choice1: "grass",
+        choice2: "fire",
+        choice3: "steel",
+        choice4: "bug",
+        answer: 1,
+    },
+    {
+        question: "?",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: 1,
+    },
+    {
+        question: "?",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: 1,
+    },
+];
